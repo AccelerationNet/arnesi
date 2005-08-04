a677aa7033f31fc07dbd79c3504cd38c0bbe51f8
Marco Baringer <mb@bese.it>
2005-08-04 11:27:27 -0400

    Fixup lexical-variables and lexical-functions for OpenMCL
    This patch causes lexical-variables to no longer return ignored
    variables and symbol-macrolets. We've also implemented
    lexical-functions (though we do some hackery to convert functions
    names to something "normal" (ie SETF::|FOO::BAR|
    
    darcs-hash:20050804152727-dd2a1-f7554fdbabb0041ac281f280f27c6be866085661.gz

--- src/lexenv.lisp
+++ src/lexenv.lisp
@@ -19,16 +19,42 @@
                 (not (ccl::istruct-typep env 'ccl::definition-environment)))
      for vars = (ccl::lexenv.variables env)
      when (listp vars)
-     append (mapcar (lambda (var)
-                      ;; ccl::var-name is a macro, se we can't do #'ccl::var-name directly
-                      (ccl::var-name var))
-                    vars)))  
+     ;; we now weed out all symbol-macros and ignored variables
+     append (remove-if (lambda (var-name)
+                         (let ((decs (assoc var-name (ccl::lexenv.vdecls env))))
+                           (and decs
+                                (eql 'cl:ignore (second decs))
+                                (eql 'cl:t (cddr decs)))))
+                       (mapcar (lambda (var)
+                                 ;; ccl::var-name is a macro, se we can't do #'ccl::var-name directly
+                                 (ccl::var-name var))
+                               (remove-if (lambda (var-spec)
+                                            (and (ccl::var-ea var-spec)
+                                                 (consp (ccl::var-ea var-spec))
+                                                 (eql :symbol-macro (car (ccl::var-ea var-spec)))))
+                                          vars)))))  
 
-;fixme 
 #+openmcl
 (defun lexical-functions (environment)
-  (declare (ignore environment))
-  nil)
+    (loop
+     for env = environment
+          then (ccl::lexenv.parent-env env)
+     while (and env
+                (not (ccl::istruct-typep env 'ccl::definition-environment)))
+     for funs = (ccl::lexenv.functions env)
+     when (listp funs)
+     ;; we now weed out all symbol-macros and ignored variables
+     append (mapcar (lambda (func-spec)
+                      ;; convert the function name to a "real" function name
+                      (let ((name (first func-spec)))
+                        (if (eql (symbol-package (first func-spec))
+                                 (find-package :SETF))
+                            (list 'cl:setf (read-from-string (symbol-name name)))
+                            name)))
+                      (remove-if (lambda (func-spec)
+                                   ;; weed out all the macrolets
+                                   (eql 'ccl::macro (second func-spec)))
+                                 funs))))
 
 ;;;; ** SBCL
  
