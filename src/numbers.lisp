acead43e3b31d8ea7e1dfc153e1dc97dd9fc4e49
Marco Baringer <mb@bese.it>
2005-10-18 06:19:50 -0400

    Rewrote parse-float in order to support tho :decimal-character keyword parameter
    darcs-hash:20051018101950-dd2a1-ed3114f6307f21ab6202a4bd8e236d5f3a41bda7.gz

--- src/numbers.lisp
+++ src/numbers.lisp
@@ -14,16 +14,70 @@
                       for frac = (* (/ n) (ldb (byte 1 i) u64))
                       sum frac)))))
 
-(defun parse-float (string
+(parse-integer)
+
+(defun radix-values (radix)
+  (assert (<= 2 radix 35)
+          (radix)
+          "RADIX must be between 2 and 35 (inclusive), not ~D." radix)
+  (make-array radix
+              :displaced-to "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+              :displaced-index-offset 0))
+
+(defun parse-float (float-string
                     &key (start 0) (end nil) (radix 10)
-                         (type 'single-float ))
-  (let* ((*read-eval* nil)
-	 (*read-base* radix)
-	 (value (read-from-string string nil nil
-                                  :start start :end end)))
-    (if (and value (numberp value))
-	(coerce value type)
-	nil)))
+                         (junk-allowed t)
+                         (type 'single-float)
+                         (decimal-character #\.))
+  (let ((radix-array (radix-values radix))
+        (integer-part 0)
+        (mantissa 0)
+        (mantissa-size 1)
+        (sign 1))
+    (with-input-from-string (float-stream (string-upcase (trim-string float-string)) :start start :end end)
+      (labels ((peek () (peek-char nil float-stream nil nil nil))
+               (next () (read-char float-stream nil nil nil))
+               (sign () ;; reads the (optional) sign of the number
+                 (cond
+                   ((char= (peek) #\+) (next) (setf sign 1))
+                   ((char= (peek) #\-) (next) (setf sign -1)))
+                 (integer-part))
+               (integer-part ()
+                 (cond
+                   ((position (peek) radix-array)
+                    ;; the next char is a valid char
+                    (setf integer-part (+ (* integer-part radix)
+                                          (position (next) radix-array)))
+                    ;; again
+                    (return-from integer-part (integer-part)))
+                   ((null (peek))
+                    ;; end of string
+                    (done))
+                   ((char= decimal-character (peek))
+                    ;; the decimal seperator
+                    (next)
+                    (return-from integer-part (mantissa)))                   
+                   ;; junk
+                   (junk-allowed (done))
+                   (t (bad-string))))
+               (mantissa ()                 
+                 (cond
+                   ((position (peek) radix-array)
+                    (setf mantissa (+ (* mantissa radix)
+                                      (position (next) radix-array))
+                          mantissa-size (* mantissa-size radix))
+                    (return-from mantissa
+                      (mantissa)))
+                   ((or (null (peek)) junk-allowed)
+                    ;; end of string
+                    (done))
+                   (t (bad-string))))
+               (bad-string ()
+                 (error "Unable to parse ~S." float-string))
+               (done ()
+                 (return-from parse-float
+                   (coerce (* sign (+ integer-part (/ mantissa mantissa-size))) type))))
+        (sign)))))
     
 (define-modify-macro mulf (delta)
   *
