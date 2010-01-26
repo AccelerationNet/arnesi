e06266b7ba72801c68dd62920df7f3286f03e57c
Russ Tyndall <russ@acceleration.net>
2010-01-26 17:02:45 -0500

    made parse float handle numbers and nil as values passed to it without erroring

--- src/numbers.lisp
+++ src/numbers.lisp
@@ -27,58 +27,65 @@
 
 (defun parse-float (float-string
                     &key (start 0) (end nil) (radix 10)
-                         (junk-allowed t)
-                         (type 'single-float)
-                         (decimal-character #\.))
-  (let ((radix-array (radix-values radix))
-        (integer-part 0)
-        (mantissa 0)
-        (mantissa-size 1)
-        (sign 1))
-    (with-input-from-string (float-stream (string-upcase (string-trim '(#\Space #\Tab) float-string)) :start start :end end)
-      (labels ((peek () (peek-char nil float-stream nil nil nil))
-               (next () (read-char float-stream nil nil nil))
-               (sign () ;; reads the (optional) sign of the number
-                 (cond
-                   ((char= (peek) #\+) (next) (setf sign 1))
-                   ((char= (peek) #\-) (next) (setf sign -1)))
-                 (integer-part))
-               (integer-part ()
-                 (cond
-                   ((position (peek) radix-array)
-                    ;; the next char is a valid char
-                    (setf integer-part (+ (* integer-part radix)
-                                          (position (next) radix-array)))
-                    ;; again
-                    (return-from integer-part (integer-part)))
-                   ((null (peek))
-                    ;; end of string
-                    (done))
-                   ((char= decimal-character (peek))
-                    ;; the decimal seperator
-                    (next)
-                    (return-from integer-part (mantissa)))                   
-                   ;; junk
-                   (junk-allowed (done))
-                   (t (bad-string))))
-               (mantissa ()                 
-                 (cond
-                   ((position (peek) radix-array)
-                    (setf mantissa (+ (* mantissa radix)
-                                      (position (next) radix-array))
-                          mantissa-size (* mantissa-size radix))
-                    (return-from mantissa
-                      (mantissa)))
-                   ((or (null (peek)) junk-allowed)
-                    ;; end of string
-                    (done))
-                   (t (bad-string))))
-               (bad-string ()
-                 (error "Unable to parse ~S." float-string))
-               (done ()
-                 (return-from parse-float
-                   (coerce (* sign (+ integer-part (/ mantissa mantissa-size))) type))))
-        (sign)))))
+		    (junk-allowed t)
+		    (type 'double-float)
+		    (decimal-character #\.))
+  (typecase float-string
+    (float float-string)
+    (null nil)
+    (number (float float-string (ecase type
+				  (single-float 0.0)
+				  ((float double-float) 0.0d0))))
+    (string
+       (let ((radix-array (radix-values radix))
+	     (integer-part 0)
+	     (mantissa 0)
+	     (mantissa-size 1)
+	     (sign 1))
+	 (with-input-from-string (float-stream (string-upcase (string-trim '(#\Space #\Tab) float-string)) :start start :end end)
+	   (labels ((peek () (peek-char nil float-stream nil nil nil))
+		    (next () (read-char float-stream nil nil nil))
+		    (sign () ;; reads the (optional) sign of the number
+		      (cond
+			((char= (peek) #\+) (next) (setf sign 1))
+			((char= (peek) #\-) (next) (setf sign -1)))
+		      (integer-part))
+		    (integer-part ()
+		      (cond
+			((position (peek) radix-array)
+			 ;; the next char is a valid char
+			 (setf integer-part (+ (* integer-part radix)
+					       (position (next) radix-array)))
+			 ;; again
+			 (return-from integer-part (integer-part)))
+			((null (peek))
+			 ;; end of string
+			 (done))
+			((char= decimal-character (peek))
+			 ;; the decimal seperator
+			 (next)
+			 (return-from integer-part (mantissa)))
+			;; junk
+			(junk-allowed (done))
+			(t (bad-string))))
+		    (mantissa ()
+		      (cond
+			((position (peek) radix-array)
+			 (setf mantissa (+ (* mantissa radix)
+					   (position (next) radix-array))
+			       mantissa-size (* mantissa-size radix))
+			 (return-from mantissa
+			   (mantissa)))
+			((or (null (peek)) junk-allowed)
+			 ;; end of string
+			 (done))
+			(t (bad-string))))
+		    (bad-string ()
+		      (error "Unable to parse ~S." float-string))
+		    (done ()
+		      (return-from parse-float
+			(coerce (* sign (+ integer-part (/ mantissa mantissa-size))) type))))
+	     (sign)))))))
     
 (define-modify-macro mulf (B)
   *
